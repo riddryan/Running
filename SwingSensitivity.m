d8e3303e9f76528525ee1d01bad59f8530e6063a
Ryan <ryridry@gmail.com>
2015-06-11 12:17:31 -0400

    Get Eigvals of Return map as fcn of pert size
    Extreme nonlinearities in the return map, so calculated a return map with differing magnitudes and signs to investigate how they are effected.
    The result is that small perturbations in certain direction cause large changes in the state of the next step due to the foot barely missing the ground and then oscillating back upwards.
    Possible ways to deal with this:
    1)  Have a locking mechanism when the swing leg reaches full extension
    2)  Include some kind of control mechanism such as inputting or removing energy

--- SwingSensitivity.m
+++ SwingSensitivity.m
@@ -16,11 +16,44 @@ loadfolder = '.\SavedGaits\Swing\';
 %% Nominal Swing Model (No impulse)
 
 loadname = 'Swing1.mat';
+savename = 'PertSize.mat';
+
 load([loadfolder loadname]);
+savefolder = ['.\ReturnMapStudies\' class(r) '\'];
 
-parameters = {'g' 'swingl' 'hipl' 'kswing' 'khip'};
-[A, B] = r.linearizeOneStepReturnMap(xstar, parameters);
+r.useHSevent = 1;
 
-[V,D] = eig(A);
-eigvals = diag(D);
+parameters = {'gslope' 'g' 'swingl' 'hipl' 'kswing' 'khip'};
 
+perts = linspace(-1e-2,1e-2,20);
+perts(perts==0)=[];
+
+A = zeros(4,4,length(perts));
+B = zeros(length(parameters),4,length(perts));
+V = A;
+D = A;
+eigvals = zeros(4,length(perts));
+
+for i = 1:length(perts) 
+    [A(:,:,i),B(:,:,i)] = r.linearizeOneStepReturnMap(xstar, parameters,'perturbationAmount',perts(i));
+    
+    [V(:,:,i),D(:,:,i)] = eig(A(:,:,i));
+    eigvals(:,i) = diag(D(:,:,i));
+end
+
+figure
+subplot(221)
+plot(perts,eigvals(1,:));
+ylabel('eigenvalue')
+subplot(222)
+plot(perts,eigvals(2,:));
+subplot(223)
+plot(perts,eigvals(3,:));
+xlabel('Perturbation Amt')
+ylabel('eigenvalue')
+subplot(224)
+plot(perts,eigvals(4,:));
+xlabel('Perturbation Amt')
+
+
+save([savefolder savename]);
